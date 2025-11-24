92c2b59c04ca3839bd57461360f4b872e8fd120b
Jasmine Falk <jyf228@nyu.edu>
2025-11-24 01:20:46 -0500

    set up extendddg to be wrapper of autoddg

---
+++ src/extendddg/utils/__init__.py
@@ -0,0 +1,16 @@
+# Re-export AutoDDG utils to allow users to import from ExtendDDG only
+from autoddg.utils import (
+    get_log_time,
+    get_sample,
+    get_various_descriptions,
+    load_prompts,
+    log_print,
+)
+
+__all__ = [
+    "get_log_time",
+    "get_sample",
+    "get_various_descriptions",
+    "log_print",
+    "load_prompts",
+]
